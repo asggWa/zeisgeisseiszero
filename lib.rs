ae73e833ff9f982a7b2f795f97cb9df8e80a0416
Alfonso Sandro Gómez Gómez <79282759+asggWa@users.noreply.github.com>
2023-12-24 23:36:23 +0100

    Update lib.rs (#2)
    To merge solution

--- lib.rs
+++ lib.rs
@@ -2,7 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right + 1/ left + right
 }
 pub fn treebtc()->i32{
+    <min js>
     print!("Buscar errores y listar para arranque seguro.")
+    print!(?php proccess("{RESPUESTA JS}"); <¡html>
+    
 }
 
 #[cfg(test)]
@@ -14,4 +17,6 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+    <div><crachtemele.io>&<%%%<scriptjsphp.web3>></div>
+
 }
